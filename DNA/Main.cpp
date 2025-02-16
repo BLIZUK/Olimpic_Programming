c5e6c9c986a4a9af32c6daa82887bc31ba8700a7
Andrei Blizuchenko <blizu4enko.andrei@yandex.ru>
2025-02-16 23:21:43 +0300

    Добавление функции сплита для строкого вектора. Проверка работаспособности.

--- DNA/Main.cpp
+++ DNA/Main.cpp
@@ -3,19 +3,51 @@
 */
 
 
+#include <sstream>
+#include <string>
 #include <iostream>
+#include <vector>
 
 
 using namespace std;
 
 
-void deepVoid()
+//vector <string>
+void split(const string& input, vector<string>& words)
 {
-	
+	istringstream iss(input);
+	//vector<string> words;
+	string word;
+
+	while (iss >> word)
+	{
+		words.push_back(word);
+	}
+
+	//return words;
+}
+
+
+void proces()
+{
+	int N, a, b;
+	string input;
+	vector<string> words;
+	cin >> N;
+	cin.ignore(); // Игнорируем оставшийся символ новой строки после ввода N
+	getline(cin, input); // Считываем всю строку
+
+	split(input, words);
+
+	for (int i = 0; i < N - 1; i++)
+	{
+
+	}
 }
 
 
 int main()
 {
+	proces();
 	return 0;
 }
